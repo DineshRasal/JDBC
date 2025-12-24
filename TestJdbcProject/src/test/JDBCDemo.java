d50c7464871a541466ba73e953807275cdb3aad5
Dinesh Rasal <rushirasal2679@gmail.com>
2025-12-24 11:14:51 +0530

    Database Conectivity

--- TestJdbcProject/src/test/JDBCDemo.java
+++ TestJdbcProject/src/test/JDBCDemo.java
@@ -12,7 +12,19 @@ public class JDBCDemo {
 				
 			//STEP 2:   CREATE CONNECTION
 				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaDB","root","12345");
+			
+			// STEP 3:  CREATE STATEMENT
+				Statement stmt=con.createStatement();
 				
+			// STEP 4: EXECUTE QUERY
+				ResultSet rs=stmt.executeQuery("Select * from person");
+				
+				while(rs.next()) {
+					System.out.println("ID: "+rs.getInt(1)+" Name:"+rs.getString(2)+" Age:"+rs.getInt(3));
+				}
+			
+			//STEP 5:  CLOSE CONNECTION
+				con.close();
 		}catch(Exception e) {
 			System.out.println(e);
 		}
