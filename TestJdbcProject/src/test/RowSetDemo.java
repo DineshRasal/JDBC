a8149b2ad0d10866aa500a6a2126690a18fa5724
Dinesh Rasal <rushirasal2679@gmail.com>
2026-01-05 10:49:18 +0530

    Configuration Of Servlet

--- TestJdbcProject/src/test/RowSetDemo.java
+++ TestJdbcProject/src/test/RowSetDemo.java
@@ -1,29 +1,29 @@
-package test;
-
-import javax.sql.rowset.CachedRowSet;
-import javax.sql.rowset.RowSetProvider;
-
-public class RowSetDemo {
-
-    public static void main(String[] args) {
-        try {
-            CachedRowSet rowset = RowSetProvider.newFactory().createCachedRowSet();
-
-            rowset.setUrl("jdbc:mysql://localhost:3306/javaDB");
-            rowset.setUsername("root");
-            rowset.setPassword("12345");
-            rowset.setCommand("select * from person");
-            rowset.execute();
-
-            while (rowset.next()) {
-                System.out.println(
-                    "ID: " + rowset.getInt(1) +
-                    " Name: " + rowset.getString(2) +
-                    " Age: " + rowset.getInt(3)
-                );
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-}
+//package test;
+//
+//import javax.sql.rowset.CachedRowSet;
+//import javax.sql.rowset.RowSetProvider;
+//
+//public class RowSetDemo {
+//
+//    public static void main(String[] args) {
+//        try {
+//            CachedRowSet rowset = RowSetProvider.newFactory().createCachedRowSet();
+//
+//            rowset.setUrl("jdbc:mysql://localhost:3306/javaDB");
+//            rowset.setUsername("root");
+//            rowset.setPassword("12345");
+//            rowset.setCommand("select * from person");
+//            rowset.execute();
+//
+//            while (rowset.next()) {
+//                System.out.println(
+//                    "ID: " + rowset.getInt(1) +
+//                    " Name: " + rowset.getString(2) +
+//                    " Age: " + rowset.getInt(3)
+//                );
+//            }
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+//    }
+//}
