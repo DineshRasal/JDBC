9d1ec2e596ebe16a94c674dc477f65a08543781a
Dinesh Rasal <rushirasal2679@gmail.com>
2026-01-09 12:10:45 +0530

    .

---
+++ UrlDemo/src/main/java/Com/FirstServlet.java
@@ -0,0 +1,54 @@
+package Com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			response.setContentType("text/html");
+			PrintWriter pw=response.getWriter();
+			
+			String  s=request.getParameter("username");
+			pw.println("Welcome"+s);
+			
+			pw.print("<a href='ServletTwo?uTest="+s+"'>Visit</a>");
+			
+			pw.close();
+		}catch(Exception e) {
+			System.out.println(e);		}
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
